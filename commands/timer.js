1796a24d29ec55aedacff9299e1f1bc49a843408
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2022-11-24 16:49:39 +0800

    reword pings > ping timers

--- commands/timer.js
+++ commands/timer.js
@@ -83,7 +83,7 @@ module.exports = {
 		let datum = new Date(Date.UTC(year + years, month + months, day + days + weeks * 7, hour + hours, minute + minutes, second + seconds - 10)) // I have no reason why the seconds seem to be 10 seconds ahead by default
 
 		if (interaction.options.getBoolean('ping') && (datum.getTime() - d.getTime()) > 31536000000) {
-			return interaction.reply({ content: 'Pings can only be valid for a year.', ephemeral: true })
+			return interaction.reply({ content: 'Ping timers can only be valid for a year.', ephemeral: true })
 		}
 
 		if (isNaN(datum.getTime())) {
