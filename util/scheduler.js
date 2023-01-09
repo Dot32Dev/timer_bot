33d20c3f10faaae0fc34bab3b34775b512a0a378
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2023-01-09 19:50:24 +0800

    Revert "Merge pull request #4 from Dot32IsCool/time-desync-fix"
    This reverts commit e6db9650e0fea2c6e6b07ba2aa0c8b762c103873, reversing
    changes made to b6a2fe9163d7cfaa31675a88e1aec1bc2ae309f9.

--- util/scheduler.js
+++ util/scheduler.js
@@ -11,7 +11,6 @@
 const { TextChannel } = require('discord.js')
 const fs = require('fs')
 const client = require('../index')
-const timeSync = require('./ntp')
 module.exports = {
     createPingBackEvent,
     checkSchedules
@@ -37,7 +36,7 @@ async function checkSchedules() {
     const schedule = readScheduleFile()
     schedule.events.forEach(async event => {
         //check if event is in the past, then run it and remove it from the schedule
-        if (event.date < (await timeSync.getTime()).now) {
+        if (event.date + 10000 < Date.now()) {
             const index = schedule.events.indexOf(event)
             schedule.events.splice(index, 1)
             handleEvent(event)
